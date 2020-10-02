4cbafc6e17d89837b62c23f3466081f5a6659e01
MJ95 <mbhilkar1995@gmail.com>
2020-10-02 13:12:44 +0530

    Update randomize.js

--- randomize.js
+++ randomize.js
@@ -3,4 +3,9 @@ function displayRandomNumber()
 {    
    let y = Math.random()
    document.getElementById("result").value = y 
-} 
+}
+//function that clear the display 
+function clr() 
+{ 
+    document.getElementById("result").value = "" 
+}
